8a09a5d85b0085daf2911c37e88d19fd693145a6
@nsengiyumv33 <nsengiyumv33@gmail.com>
2022-06-16 16:49:58 +0200

    feat: display frames

--- js/room.js
+++ js/room.js
@@ -1,19 +1,19 @@
-let messagesContainer = document.getElementById('messages');
+let messagesContainer = document.getElementById("messages");
 messagesContainer.scrollTop = messagesContainer.scrollHeight;
 
-const memberContainer = document.getElementById('members__container');
-const memberButton = document.getElementById('members__button');
+const memberContainer = document.getElementById("members__container");
+const memberButton = document.getElementById("members__button");
 
-const chatContainer = document.getElementById('messages__container');
-const chatButton = document.getElementById('chat__button');
+const chatContainer = document.getElementById("messages__container");
+const chatButton = document.getElementById("chat__button");
 
 let activeMemberContainer = false;
 
-memberButton.addEventListener('click', () => {
+memberButton.addEventListener("click", () => {
   if (activeMemberContainer) {
-    memberContainer.style.display = 'none';
+    memberContainer.style.display = "none";
   } else {
-    memberContainer.style.display = 'block';
+    memberContainer.style.display = "block";
   }
 
   activeMemberContainer = !activeMemberContainer;
@@ -21,12 +21,29 @@ memberButton.addEventListener('click', () => {
 
 let activeChatContainer = false;
 
-chatButton.addEventListener('click', () => {
+chatButton.addEventListener("click", () => {
   if (activeChatContainer) {
-    chatContainer.style.display = 'none';
+    chatContainer.style.display = "none";
   } else {
-    chatContainer.style.display = 'block';
+    chatContainer.style.display = "block";
   }
 
   activeChatContainer = !activeChatContainer;
 });
+
+let displayFrame = document.getElementById("stream__box");
+let videoFrames = document.getElementsByClassName("video__container");
+let userIdInDisplayFrame = null;
+
+let expandVideoFrame = (e) => {
+
+  let child = displayFrame.children[0]
+  if(child){
+    document.getElementById('streams__container').appendChild(child)
+  }
+  displayFrame.style.display = "block";
+};
+
+for (let i = 0; videoFrames.length > i; i++) {
+  videoFrames[i].addEventListener("click", expandVideoFrame);
+}
