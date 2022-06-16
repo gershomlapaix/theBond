428e373c264e603dbbf5f00f3ee6f5864df6d413
@nsengiyumv33 <nsengiyumv33@gmail.com>
2022-06-16 16:58:32 +0200

    feat: style frames

--- js/room.js
+++ js/room.js
@@ -36,12 +36,23 @@ let videoFrames = document.getElementsByClassName("video__container");
 let userIdInDisplayFrame = null;
 
 let expandVideoFrame = (e) => {
+  let child = displayFrame.children[0];
 
-  let child = displayFrame.children[0]
-  if(child){
-    document.getElementById('streams__container').appendChild(child)
+  if (child) {
+    document.getElementById("streams__container").appendChild(child);
   }
+
   displayFrame.style.display = "block";
+  displayFrame.appendChild(e.currentTarget);
+  userIdInDisplayFrame = e.currentTarget.id;
+
+  for (let i = 0; videoFrames.length > i; i++) {
+    if (videoFrames[i].id != userIdInDisplayFrame) {
+      videoFrames[i].style.height = "100px";
+      videoFrames[i].style.width = "100px";
+      console.log(videoFrames[i].id);
+    }
+  }
 };
 
 for (let i = 0; videoFrames.length > i; i++) {
